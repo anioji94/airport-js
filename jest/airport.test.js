9c4ae5c6c2f6fbfe7ec6da95b1aa665e12be0a4d
Aniruddh <ojhaa94@gmail.com>
2021-05-11 11:15:07 +0100

    adds capacity,  prevents landing when full

--- jest/airport.test.js
+++ jest/airport.test.js
@@ -5,8 +5,27 @@
 const { expect } = require('@jest/globals');
 const Airport = require('../src/airport');
 const airport = require('../src/airport')
+const testAirport = new Airport
+const smallAirport = new Airport(1, 1)
 
+// .INITIALIZE
+test('airport capacity can be overriden', () => {
+	const newAirport = new Airport
+	expect(newAirport.capacity).toBe(5)
+	newAirport.capacity = 6
+	expect(newAirport.capacity).toBe(6)
+});
+
+// .LAND(PLANE)
 test('airports can instruct planes to land', () => {
-	testAirport = new Airport
 	expect(testAirport.landPlane()).toBe('plane has landed');
+});
+
+test('airports can prevent landing when capacity is full', () => {
+	expect(smallAirport.landPlane()).toBe('request denied, airport full')	
+})
+
+// .TAKEOFF(PLANE)
+test('airports can instruct planes to take off', () => {
+	expect(testAirport.takeOff()).toBe('plane has taken off')
 });
--- src/airport.js
+++ src/airport.js
@@ -1,9 +1,19 @@
 class Airport {
-	constructor() {
-
+	constructor(cap = 5, num = 0) {
+		this.capacity = cap,
+		this.hangar = num
 	}
-	landPlane() {
-		return `plane has landed`
+	landPlane(plane = "plane") {
+		if (this.hangar < this.capacity) {
+			this.hangar += 1
+			return `${plane} has landed`
+		} else {
+			return 'request denied, airport full'
+		}
+	}
+
+	takeOff(plane = "plane") {
+		return `${plane} has taken off`
 	}
 }
 
